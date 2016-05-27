da08b4c3d1678a3202affe65f3b2d55f3ee92ea1
Gitxiangwang <2278368772@qq.com>
2016-05-27 17:09:22 +0800

    更改后的ReaderMe第二次提交

--- ReaderMe/ReaderMe/Defines.h
+++ ReaderMe/ReaderMe/Defines.h
@@ -6,8 +6,24 @@
 //  Copyright © 2016年 www.zhuanqiantouzi.com. All rights reserved.
 //
 
-#ifndef Defines_h
-#define Defines_h
+#if (DEBUG || TESTCASE)
+#define XWLog(format, ...) NSLog(format, ## __VA_ARGS__)
+#else
+#define FxLog(format, ...)
+#endif
 
+// 日志输出宏
+#define BASE_LOG(cls,sel) FxLog(@"%@-%@",NSStringFromClass(cls), NSStringFromSelector(sel))
+#define BASE_ERROR_LOG(cls,sel,error) FxLog(@"ERROR:%@-%@-%@",NSStringFromClass(cls), NSStringFromSelector(sel), error)
+#define BASE_INFO_LOG(cls,sel,info) FxLog(@"INFO:%@-%@-%@",NSStringFromClass(cls), NSStringFromSelector(sel), info)
 
-#endif /* Defines_h */
+// 日志输出函数
+#if (DEBUG || TESTCASE)
+#define BASE_LOG_FUN()         BASE_LOG([self class], _cmd)
+#define BASE_ERROR_FUN(error)  BASE_ERROR_LOG([self class],_cmd,error)
+#define BASE_INFO_FUN(info)    BASE_INFO_LOG([self class],_cmd,info)
+#else
+#define BASE_LOG_FUN()
+#define BASE_ERROR_FUN(error)
+#define BASE_INFO_FUN(info)
+#endif
